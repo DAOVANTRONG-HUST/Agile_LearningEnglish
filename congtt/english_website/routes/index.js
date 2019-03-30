681b85ea00ab92fe8b13671de02de5c6a63fb74b
ThanhCong598 <trinhthanhcong98@gmail.com>
2019-03-30 21:27:40 +0700

    dang nhap-dang ky tai khoan

--- congtt/english_website/routes/index.js
+++ congtt/english_website/routes/index.js
@@ -10,25 +10,127 @@ const assert = require('assert');
 const url = 'mongodb://localhost:27017';
 
 // Database Name
-const dbName = 'contact';
+const dbName = 'englishWebsite';
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+/* GET register */
+router.get('/register', function (req, res, next) {
+  res.render('login/register', { data: {} });
+});
+
+router.post("/register", function (req, res, next) {
+  var data = req.body;
+
+  if (data.email.trim().length == 0) {
+    res.render('login/register', { data: { error: "Email is require" } });
+  } else if (data.passwd != data.repasswd && data.passwd.trim().length != 0) {
+    res.render('login/register', { data: { error: "Password not match" } });
+  } else {
+
+    user = {
+      email: data.email,
+      password: data.passwd,
+      first_name: data.firstname,
+      last_name: data.lastname
+
+    };
+    const insertDocuments = function (db, callback) {
+      // Get the documents collection
+      const collection = db.collection('hocvien');
+      // Insert some documents
+      collection.insert(user, function (err, result) {
+        assert.equal(err, null);
+        console.log("Inserted into the collection");
+        callback(result);
+      });
+    }
+
+    // Use connect method to connect to the server
+    MongoClient.connect(url, function (err, client) {
+      assert.equal(null, err);
+      console.log("Connected successfully to server");
+
+      const db = client.db(dbName);
+
+      insertDocuments(db, function () {
+        client.close();
+      });
+    });
+    res.redirect("/login");
+
+  }
+})
+
+/* GET  login*/
+router.get('/login', function (req, res, next) {
+  res.render("login/login", { data: {} });
+});
+
+// Post login
+router.post("/login", function (req, res, next) {
+  var params = req.body;
+
+  if (params.email.trim().length == 0) {
+    res.render("login/login", { data: { error: "Please enter an email" } });
+  } else {
+
+    //ham tim kiem
+    const findDocuments = function (db, callback) {
+      // Get the documents collection
+      const collection = db.collection('hocvien');
+      // Find some documents
+      collection.find({ email: params.email }).toArray(function (err, docs) {
+        assert.equal(err, null);
+        console.log("Found the following records");
+        console.log(docs);
+        callback(docs);
+      });
+    }
+
+    MongoClient.connect(url, function (err, client) {
+      assert.equal(null, err);
+      console.log("Connected correctly to server");
+
+      const db = client.db(dbName);
+
+
+      findDocuments(db, function (dulieu) {
+        console.log(dulieu);
+        var user=dulieu[0];
+        if(user.password===params.password){
+          console.log("login sucess");
+        }else{
+          res.render("login/login", { data: { error: "Password is not correct" } });
+        }
+        client.close();
+      });
+
+    });
+
+    
+  }
+})
+
+
+
+
 /* GET thêm dữ liệu */
 router.get('/them', function (req, res, next) {
-  res.render('them', { data: {} });
+  res.render('CRUD/them', { data: {} });
 });
 
 /* Post thêm dữ liệu */
 router.post('/them', function (req, res, next) {
   var data = req.body;
   if (!data.ten) {
-    res.render("them", { data: { err: "Name is not correct" } });
+    res.render('CRUD/them', { data: { err: "Name is not correct" } });
   } else if (!data.dt) {
-    res.render("them", { data: { err: "Phone is not correct" } });
+    res.render('CRUD/them', { data: { err: "Phone is not correct" } });
   } else {
 
 
@@ -59,7 +161,7 @@ router.post('/them', function (req, res, next) {
         client.close();
       });
     });
-    res.render("them", { data: { err: "Thêm dữ liệu thành công" } });
+    res.render('CRUD/them', { data: { err: "Thêm dữ liệu thành công" } });
 
   }
 
@@ -89,7 +191,7 @@ router.get('/xem', function (req, res, next) {
 
 
     findDocuments(db, function (dulieu) {
-      res.render('xem', { data: dulieu });
+      res.render('CRUD/xem', { data: dulieu });
       client.close();
     });
 
@@ -98,44 +200,44 @@ router.get('/xem', function (req, res, next) {
 });
 
 /* Xoa du lieu . */
-router.get('/xoa/:idcanxoa', function(req, res, next) {
-  var idcanxoa = chuyenthanhObjectId(req.params.idcanxoa); 
+router.get('/xoa/:idcanxoa', function (req, res, next) {
+  var idcanxoa = chuyenthanhObjectId(req.params.idcanxoa);
   //ham xoa 
-  const xoacontact = function(db, callback) {
-     const collection = db.collection('nguoidung');
-     collection.deleteOne({ _id : idcanxoa }, function(err, result) {
+  const xoacontact = function (db, callback) {
+    const collection = db.collection('nguoidung');
+    collection.deleteOne({ _id: idcanxoa }, function (err, result) {
       assert.equal(err, null);
-       console.log("Xoa thanh cong ");
-      
+      console.log("Xoa thanh cong ");
+
       callback(result);
-      
-    });    
+
+    });
   }
   // ket noi mongo
-    // Use connect method to connect to the server
-    MongoClient.connect(url, function(err, client) {
-      assert.equal(null, err); 
-      const db = client.db(dbName); 
-      xoacontact(db, function() {
-            client.close();
-            res.redirect('/xem');
-          });   
+  // Use connect method to connect to the server
+  MongoClient.connect(url, function (err, client) {
+    assert.equal(null, err);
+    const db = client.db(dbName);
+    xoacontact(db, function () {
+      client.close();
+      res.redirect('/xem');
     });
+  });
 
-   
+
 });  // end xoa du lieu 
 
 // sua du lieu
 
-router.get('/sua/:idcansua', function(req, res, next) {
-  var idcansua = chuyenthanhObjectId(req.params.idcansua); 
+router.get('/sua/:idcansua', function (req, res, next) {
+  var idcansua = chuyenthanhObjectId(req.params.idcansua);
 
   //ham tim kiem
-  const findDocuments = function(db, callback) {
+  const findDocuments = function (db, callback) {
     // Get the documents collection
     const collection = db.collection('nguoidung');
     // Find some documents
-    collection.find({_id: idcansua}).toArray(function(err, docs) {
+    collection.find({ _id: idcansua }).toArray(function (err, docs) {
       assert.equal(err, null);
       console.log("Found the following records");
       console.log(docs);
@@ -152,53 +254,53 @@ router.get('/sua/:idcansua', function(req, res, next) {
 
     findDocuments(db, function (dulieu) {
       console.log(dulieu);
-      res.render('sua', { data: dulieu });
+      res.render('CRUD/sua', { data: dulieu });
       client.close();
     });
 
   });
- 
 
-   
-});  
+
+
+});
 
 
 //sua du lieu
 
-router.post('/sua/:idcansua', function(req, res, next) {
-  var idcansua = chuyenthanhObjectId(req.params.idcansua); 
+router.post('/sua/:idcansua', function (req, res, next) {
+  var idcansua = chuyenthanhObjectId(req.params.idcansua);
 
-  var dulieu01={
-    ten:req.body.ten,
-    dt:req.body.dt
+  var dulieu01 = {
+    ten: req.body.ten,
+    dt: req.body.dt
   }
 
-  const updateDocument = function(db, callback) {
+  const updateDocument = function (db, callback) {
     // Get the documents collection
     const collection = db.collection('nguoidung');
     // Update document where a is 2, set b equal to 1
     collection.updateOne({ _id: idcansua }
-      , { $set: dulieu01 }, function(err, result) {
-      assert.equal(err, null);
-      console.log("Updated ");
-      callback(result);
-    });
+      , { $set: dulieu01 }, function (err, result) {
+        assert.equal(err, null);
+        console.log("Updated ");
+        callback(result);
+      });
   }
 
   // Use connect method to connect to the server
-MongoClient.connect(url, function(err, client) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
+  MongoClient.connect(url, function (err, client) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
 
-  const db = client.db(dbName);
+    const db = client.db(dbName);
 
- 
-    updateDocument(db, function() {
+
+    updateDocument(db, function () {
       client.close();
       res.redirect('/xem');
     });
   });
 
- 
+
 });  // end sua du lieu 
 module.exports = router;
