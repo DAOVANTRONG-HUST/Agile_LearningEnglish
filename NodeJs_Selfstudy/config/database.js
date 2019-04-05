d50f24e93cb1c36462b0e7f581e462e3282d9b8d
Hoangvlhust <43082945+jokerboy606@users.noreply.github.com>
2019-04-06 00:05:31 +0700

    new file structure

--- NodeJs_Selfstudy/config/database.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    'secret': 'nodeauthsecret',
-    'database': 'mongodb+srv://hoangvlbk61:@Lehoang606@cluster0-z9rem.mongodb.net/ESS?retryWrites=true'
-};// load icon tu tren database xuống
--- NodeJs_Selfstudy/models/crt.js
+++
@@ -1,9 +0,0 @@
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb://hoangvlbk61:@Lehoang606@cluster0-z9rem.mongodb.net/test?retryWrites=true";
-
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
