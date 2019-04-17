edde887f0fb64f46891e0136766495c6675757c7
Đào Văn Trọng <daovantrong.hust@gmail.com>
2019-04-17 13:07:55 +0700

    test

---
+++ trongdv/english_website/routes/dethi.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+var dethi=require("../dbs/dethi.json")
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+
+  res.render('dethi/listening/part1',{dethi:dethi});
+
+});
+
+router.post('/submit', function (req, res) {
+    var q_list = [];
+   
+    let i=1;
+    
+    dethi.PartI.bo_cau_hoi.forEach(function (cauhoi) {
+      let a='a'+i;
+      var item = {
+        iq_id: cauhoi.id_cauhoi,
+        explain: cauhoi.explain
+      };
+  
+      if (cauhoi[a] == req.body[a]) {
+        item.result = 1;
+      } else {
+        item.result = 0;
+      }
+      i++;
+  
+      q_list.push(item);
+    })
+  
+    
+  res.json({ q_list: q_list });
+  
+  })
+
+module.exports=router;
+
